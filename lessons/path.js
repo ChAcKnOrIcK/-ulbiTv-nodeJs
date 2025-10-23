f23fd23b45a7e28a7bf3484d5ff1375b5262e1c7
ChAcKnOrIcK <sarmatmargiev28092005@gmail.com>
2025-10-23 22:16:27 +0300

    test save

--- lessons/path.js
+++ lessons/path.js
@@ -2,3 +2,7 @@ const path = require('path');
 
 console.log('Склеить участки пути', path.join(__dirname, '..', '..'));
 console.log('Склеить участки пути', path.resolve(__dirname, '..', '..'));
+
+
+
+// слова для комента и теста, просто слова 
